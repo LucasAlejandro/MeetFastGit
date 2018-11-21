e9ddf997fbcea5029bb9644d6c7955c63bb13752
LucasAlejandro <alejandrolucas9546@gmail.com>
2018-11-21 17:41:24 +0100

    Pull nº3 vendor/home

--- MeetFastGit/Controllers/HomeController.cs
+++ MeetFastGit/Controllers/HomeController.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace MeetFastEmpty.Controllers
+namespace MeetFastGit.Controllers
 {
     public class HomeController : Controller
     {
